94e115cb6dd8859f36c0b75b2ca3aacc7318610b
Godfried Vanluffelen <godfried.vanluffelen@dekimo.com>
2020-01-07 15:51:27 +0100

    Set hotend temp to 300

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -350,7 +350,7 @@
 // When temperature exceeds max temp, your heater will be switched off.
 // This feature exists to protect your hotend from overheating accidentally, but *NOT* from thermistor short/failure!
 // You should use MINTEMP for thermistor short/failure protection.
-#define HEATER_0_MAXTEMP 275
+#define HEATER_0_MAXTEMP 300    // 285 max setpoint (300 - 15). Micro swiss hotend should be able to handle this
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
